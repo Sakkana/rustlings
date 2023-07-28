3f53a52776013c3849591e075ff4d44b2a0ded84
Sakkana <642835027@qq.com>
2023-07-29 00:34:13 +0800

    7.29 variables ✅

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    let mut x = 999;
     println!("Number {}", x);
+
+    // 因为需要 x 可变，需要绑定到 mutable 的变量上
     x = 5; // don't change this line
     println!("Number {}", x);
 }
