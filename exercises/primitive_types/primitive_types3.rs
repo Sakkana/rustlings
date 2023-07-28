f3a4601fafaf04114787077865763521ea3d94b0
Sakkana <642835027@qq.com>
2023-07-29 00:54:11 +0800

    7.29 primitive_types ✅

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,16 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // 1. 直接写死法
+    // let a = [1, 2, 3, 4, 5];
+
+    // 2. 直接定义法
+    // 不可以这样写，大小给多少就要初始化多少
+    // let a: [i32; 101] = [1, 2, 3];
+
+    // 可以直接初始化成相同的值
+    let a = [0; 101];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,13 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // 对一个已经定义的数组进行切片
+    // 这里要加 & 符号来 borrow
+    let nice_slice = &a[1..=3];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
